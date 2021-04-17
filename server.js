5868c60081155c7f38827e2a8236c8f1ec5f9b5b
oussamahboubi <elmahboubi.ensias@gmail.com>
2021-04-17 16:11:41 +0000

    make it better

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/chobiklobik'));
+app.use(express.static('./dist/chobiklobik'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'src/chobiklobik/'}),
+    res.sendFile('index.html', {root: 'dist/chobiklobik/'}),
 );
 
 // Start the app by listening on the default Heroku port
